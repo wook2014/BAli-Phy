c4ca753530c76303c5724a38618e9c0f84df0e23
Benjamin Redelings <benjamin.redelings@duke.edu>
2015-05-05 22:23:48 -0400

    Iterate over attachment_branch_pairs instead.

--- src/mcmc/sample-topology-SPR.C
+++ src/mcmc/sample-topology-SPR.C
@@ -869,14 +869,14 @@ spr_attachment_probabilities SPR_search_attachment_points(Parameters& P, int b1,
   vector<owned_ptr<Parameters>> Ps;
   Ps.reserve(I.attachment_branch_pairs.size());
   Ps.push_back(P);
-  for(int i=1;i<branch_names.size();i++) 
+  for(const auto& BB: I.attachment_branch_pairs)
   {
     // Define target branch b2 - pointing away from b1
-    int b2 = branch_names[i];
-    tree_edge B2 = I.get_tree_edge(b2);
+    tree_edge B2 = BB.second;
 
-    Ps.push_back(Ps.back());
+    Ps.push_back(Ps[0]);
     auto& p = *Ps.back();
+    int b2 = p.T().directed_branch(B2);
     int BM2 = SPR_at_location(p, b1, b2, locations, I.BM);
     assert(BM2 == I.BM); // Due to the way the current implementation of SPR works, BM (not B1) should be moved.
 
