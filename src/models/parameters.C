c9857cdcb4b4228f7cb2d59b835b5b08d0e52015
Benjamin Redelings <benjamin.redelings@gmail.com>
2015-08-05 15:10:05 -0400

    Pass data_partition* into subA_index instead of Parameters*.

--- src/models/parameters.C
+++ src/models/parameters.C
@@ -203,7 +203,7 @@ bool use_internal_index = true;
 
 subA_index_t& data_partition::subA() const
 {
-  subA_->P = P;
+  subA_->DP = this;
   return *subA_;
 }
 
@@ -267,7 +267,7 @@ void data_partition::variable_alignment(bool b)
   // turning OFF alignment variation
   if (not variable_alignment()) 
   {
-    subA_ = new subA_index_leaf(P, subA_row_indices, subA_up_to_date, T().n_branches()*2);
+    subA_ = new subA_index_leaf(this, subA_row_indices, subA_up_to_date, T().n_branches()*2);
 
     // We just changed the subA index type
     LC.invalidate_all();
@@ -280,9 +280,9 @@ void data_partition::variable_alignment(bool b)
   else 
   {
     if (use_internal_index)
-      subA_ = new subA_index_internal(P, subA_row_indices, subA_up_to_date, T().n_branches()*2);
+      subA_ = new subA_index_internal(this, subA_row_indices, subA_up_to_date, T().n_branches()*2);
     else
-      subA_ = new subA_index_leaf(P, subA_row_indices, subA_up_to_date, T().n_branches()*2);
+      subA_ = new subA_index_leaf(this, subA_row_indices, subA_up_to_date, T().n_branches()*2);
 
     assert(has_IModel() and A().n_sequences() == T().n_nodes());
     {
@@ -641,9 +641,9 @@ data_partition::data_partition(Parameters* p, int i, const alignment& AA)
     subA_up_to_date[i]  = p->add_parameter(invisible_prefix + "subAU"+convertToString(i), 0);
   
   if (variable_alignment() and use_internal_index)
-    subA_ = new subA_index_internal(P, subA_row_indices, subA_up_to_date, B*2);
+    subA_ = new subA_index_internal(this, subA_row_indices, subA_up_to_date, B*2);
   else
-    subA_ = new subA_index_leaf(P, subA_row_indices, subA_up_to_date, B*2);
+    subA_ = new subA_index_leaf(this, subA_row_indices, subA_up_to_date, B*2);
 
   alignment_matrix_index = p->add_parameter(invisible_prefix + "A", AA);
   if (variable_alignment())
