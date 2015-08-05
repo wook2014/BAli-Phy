e39c07e8aab5d9221fa32e2b76d3f817498e0100
Benjamin Redelings <benjamin.redelings@gmail.com>
2015-08-05 15:18:00 -0400

    Add A() and T() member functions to subA_index_t

--- src/substitution/substitution-index.C
+++ src/substitution/substitution-index.C
@@ -101,6 +101,16 @@ const Parameters& subA_index_t::P() const
   return *DP->P;
 }
 
+const alignment& subA_index_t::A() const
+{
+  return DP->A();
+}
+
+const Tree& subA_index_t::T() const
+{
+  return DP->T();
+}
+  
 void subA_index_t::set_row(int r, const Vector<pair<int,int>>& index)
 {
   const context* C = &P();
