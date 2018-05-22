12dbfa365601ca407934c24cfe2c4c2ac9bf3a1b
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-05-22 18:19:16 -0400

    Constrain the functions for DPmatrixSimple.

--- src/dp/dp-matrix.cc
+++ src/dp/dp-matrix.cc
@@ -499,10 +499,8 @@ void DPmatrixSimple::forward_cell_only_1(int i2,int j2)
     assert(0 < i2 and i2 < size1());
     assert(0 < j2 and j2 < size2());
 
-    prepare_cell(i2,j2);
-
     // determine initial scale for this cell
-    scale(i2,j2) = max(scale(i2-1,j2), max( scale(i2-1,j2-1), scale(i2,j2-1) ) );
+    scale(i2,j2) = scale(i2-1,j2);
 
     double maximum = 0;
 
@@ -518,20 +516,12 @@ void DPmatrixSimple::forward_cell_only_1(int i2,int j2)
 	if (di(S2)) i1--;
 
 	int j1 = j2;
-	if (dj(S2)) j1--;
 
 	//--- Compute Arrival Probability ----
 	double temp  = 0;
-	for(int S1=0;S1<n_dp_states();S1++)
-	    temp += (*this)(i1,j1,S1) * GQ(S1,S2);
-
-	//--- Include Emission Probability----
-	if (i1 != i2 and j1 != j2)
-	    temp *= emitMM(i2,j2);
-
-	// rescale result to scale of this cell
-	if (scale(i1,j1) != scale(i2,j2))
-	    temp *= pow2(scale(i1,j1)-scale(i2,j2));
+	if (not dj(S2))
+	    for(int S1=0;S1<n_dp_states();S1++)
+		temp += (*this)(i1,j1,S1) * GQ(S1,S2);
 
 	// record maximum
 	if (temp > maximum) maximum = temp;
@@ -556,10 +546,8 @@ void DPmatrixSimple::forward_cell_only_2(int i2,int j2)
     assert(0 < i2 and i2 < size1());
     assert(0 < j2 and j2 < size2());
 
-    prepare_cell(i2,j2);
-
     // determine initial scale for this cell
-    scale(i2,j2) = max(scale(i2-1,j2), max( scale(i2-1,j2-1), scale(i2,j2-1) ) );
+    scale(i2,j2) = scale(i2,j2-1);
 
     double maximum = 0;
 
@@ -572,23 +560,15 @@ void DPmatrixSimple::forward_cell_only_2(int i2,int j2)
     {
 	//--- Get (i1,j1) from (i2,j2) and S2
 	int i1 = i2;
-	if (di(S2)) i1--;
 
 	int j1 = j2;
 	if (dj(S2)) j1--;
 
 	//--- Compute Arrival Probability ----
 	double temp  = 0;
-	for(int S1=0;S1<n_dp_states();S1++)
-	    temp += (*this)(i1,j1,S1) * GQ(S1,S2);
-
-	//--- Include Emission Probability----
-	if (i1 != i2 and j1 != j2)
-	    temp *= emitMM(i2,j2);
-
-	// rescale result to scale of this cell
-	if (scale(i1,j1) != scale(i2,j2))
-	    temp *= pow2(scale(i1,j1)-scale(i2,j2));
+	if (not di(S2))
+	    for(int S1=0;S1<n_dp_states();S1++)
+		temp += (*this)(i1,j1,S1) * GQ(S1,S2);
 
 	// record maximum
 	if (temp > maximum) maximum = temp;
