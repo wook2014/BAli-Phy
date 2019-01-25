9c3cad7afeb23f438a25cb6909430440b2e988ff
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-01-24 19:52:16 -0500

    Apparently mapping values cannot be 0.

--- src/computation/machine/graph_register.cc
+++ src/computation/machine/graph_register.cc
@@ -751,7 +751,7 @@ bool reg_heap::has_result(int r) const
 
 bool reg_heap::unforced_step(int r) const
 {
-    return prog_unforced_step[r] != forced_index;
+    return prog_unforced_step[r] > 0;
 }
 
 void reg_heap::force_step(int r)
@@ -778,7 +778,7 @@ void reg_heap::force_step(int r)
 
 bool reg_heap::unforced_result(int r) const
 {
-    return prog_unforced_result[r] != forced_index;
+    return prog_unforced_result[r] > 0;
 }
 
 void reg_heap::force_result(int r)
