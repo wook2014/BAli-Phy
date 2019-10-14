054d43cd90176e4c39d048632f8e12bc16adfcce
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-10-14 18:43:18 -0400

    Add note.

--- src/computation/machine/tokens.cc
+++ src/computation/machine/tokens.cc
@@ -14,6 +14,10 @@ long total_release_knuckle = 0;
 
 void reg_heap::destroy_all_computations_in_token(int t)
 {
+    // SPEEDUP.  It might be possible to save time clearing forward/backward edges
+    //           by marking objects that will be deleted, and only fixing up objects
+    //           that are not going away.
+
     // Remove use back-edges
     auto& delta_step = tokens[t].delta_step();
     for(auto p: delta_step)
